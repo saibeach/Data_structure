f0ab10c6808e447f841bab2bcb81f287cca982c2
Alex.Xu <48598761+saibeach@users.noreply.github.com>
2021-04-23 08:17:41 -0500

    Update node.py

--- node.py
+++ node.py
@@ -3,3 +3,9 @@ class Node:
   def __init__(self, value, link_node = None):
     self.value = value
     self.link_node = link_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_link(self):
+    return self.link_node
