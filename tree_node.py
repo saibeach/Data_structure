009e7ff2c92259f366e825eb9eb5404a9db179d9
Alex.Xu <48598761+saibeach@users.noreply.github.com>
2021-05-06 23:33:40 -0500

    Add files via upload
    tree_node_example

--- tree_node.py
+++ tree_node.py
@@ -1,23 +1,80 @@
+print("Once upon a time...")
+######
 class TreeNode:
-  def __init__(self, value):
-    self.value = value # data
-    self.children = [] # references to other nodes
-
-  def add_child(self, child_node):
-    # creates parent-child relationship
-    print("Adding " + child_node.value)
-    self.children.append(child_node) 
+  def __init__(self, story_piece):
+    self.story_piece = story_piece
+    self.choices = []
     
-  def remove_child(self, child_node):
-    # removes parent-child relationship
-    print("Removing " + child_node.value + " from " + self.value)
-    self.children = [child for child in self.children 
-                     if child is not child_node]
+######
+  def add_child(self, node):
+    self.choices.append(node)
 
   def traverse(self):
-    # moves through each node referenced from self downwards
-    nodes_to_visit = [self]
-    while len(nodes_to_visit) > 0:
-      current_node = nodes_to_visit.pop()
-      print(current_node.value)
-      nodes_to_visit += current_node.children
+    story_node = self
+    print(story_node.story_piece)
+    while story_node.choices != []:
+      choice = input("Enter 1 or 2 to continue the story: ")
+      if choice != "1" and choice != "2":
+        print("Please input 1 or 2")
+      else:
+        chosen_index = int(choice) - 1
+        chosen_child = story_node.choices[chosen_index]
+        print(chosen_child.story_piece)
+        story_node = chosen_child
+
+######
+story_root = TreeNode("""
+You are in a forest clearing. There is a path to the left.
+A bear emerges from the trees and roars!
+Do you: 
+1 ) Roar back!
+2 ) Run to the left...
+""")
+user_choice = input("What is your name?")
+print(user_choice)
+choice_a = TreeNode("""
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+""")
+choice_b = TreeNode("""
+You come across a clearing full of flowers. 
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+""")
+choice_a_1 = TreeNode("""
+The bear returns and tells you it's been a rough week. After making peace with
+a talking bear, he shows you the way out of the forest.
+ 
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+choice_a_2 = TreeNode("""
+The bear returns and tells you that bullying is not okay before leaving you alone
+in the wilderness.
+ 
+YOU REMAIN LOST.
+""")
+choice_b_1 = TreeNode("""
+The bear is unamused. After smelling the flowers, it turns around and leaves you alone.
+ 
+YOU REMAIN LOST.
+""")
+choice_b_2 = TreeNode("""
+The bear understands and apologizes for startling you. Your new friend shows you a 
+path leading out of the forest.
+ 
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
+######
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
+choice_a.add_child(choice_a_1)
+choice_a.add_child(choice_a_2)
+choice_b.add_child(choice_b_1)
+choice_b.add_child(choice_b_2)
+story_root.traverse()
+######
